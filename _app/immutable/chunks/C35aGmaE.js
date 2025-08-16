6dd9da9a7fff945a962df595c88f03bff253dca9
onjmin <onjmin@users.noreply.github.com>
2025-08-16 05:19:45 +0000

    deploy: 795f4aa65f6ca71d7eba6824b1b6e752119134f1

--- _app/immutable/chunks/C35aGmaE.js
+++
@@ -1,1 +0,0 @@
-import{aa as a}from"./D-Gy2L8U.js";a();
---
+++ _app/immutable/nodes/5.y0md3IYk.js
@@ -0,0 +1,98 @@
+import"../chunks/DsnmJJEf.js";import{s as O}from"../chunks/HW9zc2Ie.js";import{K as _,L as Z,S as L,aZ as q,M as G,N as I,P as J,a_ as K,V as U,a$ as Y,X as Q,O as ee,a3 as D,a2 as F,al as te,T as ae,a5 as re,Z as se,q as k,b0 as oe,B as b,aW as w,J as N,x as h,D as S,C as g,y as C,H as u,aX as ne,Y as M,b1 as le,r as H,w as R,A as V}from"../chunks/DGrzWCOZ.js";import{i as ie}from"../chunks/BJ33f57l.js";import{e as z,i as B}from"../chunks/D5UtWgbD.js";import{c as ce}from"../chunks/CXDxQxK3.js";import{s as A}from"../chunks/BL5y9y9-.js";import{p as $,r as E,s as P}from"../chunks/CcMu7uqT.js";import{a as X}from"../chunks/D8EokeuK.js";function de(i,e,o,r,n,m){let t=_;_&&Z();var a,l,s=null;_&&L.nodeType===q&&(s=L,Z());var x=_?L:i,d;G(()=>{const c=e()||null;var f=K;c!==a&&(d&&(c===null?ae(d,()=>{d=null,l=null}):c===l?re(d):se(d)),c&&c!==l&&(d=J(()=>{if(s=_?s:document.createElementNS(f,c),U(s,s),r){_&&Y(c)&&s.append(document.createComment(""));var v=_?Q(s):s.appendChild(ee());_&&(v===null?D(!1):F(v)),r(s,v)}te.nodes_end=s,x.before(s)})),a=c,a&&(l=a))},I),t&&(D(!0),F(x))}/**
+ * @license @lucide/svelte v0.539.0 - ISC
+ *
+ * ISC License
+ * 
+ * Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2022 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2022.
+ * 
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * 
+ */const fe={xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor","stroke-width":2,"stroke-linecap":"round","stroke-linejoin":"round"};var ve=oe("<svg><!><!></svg>");function W(i,e){k(e,!0);const o=$(e,"color",3,"currentColor"),r=$(e,"size",3,24),n=$(e,"strokeWidth",3,2),m=$(e,"absoluteStrokeWidth",3,!1),t=$(e,"iconNode",19,()=>[]),a=E(e,["$$slots","$$events","$$legacy","name","color","size","strokeWidth","absoluteStrokeWidth","iconNode","children"]);var l=ve();X(l,d=>({...fe,...a,width:r(),height:r(),stroke:o(),"stroke-width":d,class:["lucide-icon lucide",e.name&&`lucide-${e.name}`,e.class]}),[()=>m()?Number(n())*24/Number(r()):n()]);var s=b(l);z(s,17,t,B,(d,c)=>{var f=ne(()=>le(u(c),2));let v=()=>u(f)[0],p=()=>u(f)[1];var y=w(),T=N(y);de(T,v,!0,(j,ye)=>{X(j,()=>({...p()}))}),h(d,y)});var x=S(s);A(x,()=>e.children??M),g(l),h(i,l),C()}function ue(i,e){k(e,!0);/**
+ * @license @lucide/svelte v0.539.0 - ISC
+ *
+ * ISC License
+ *
+ * Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2022 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2022.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */let o=E(e,["$$slots","$$events","$$legacy"]);const r=[["path",{d:"M10.268 21a2 2 0 0 0 3.464 0"}],["path",{d:"M3.262 15.326A1 1 0 0 0 4 17h16a1 1 0 0 0 .74-1.673C19.41 13.956 18 12.499 18 8A6 6 0 0 0 6 8c0 4.499-1.411 5.956-2.738 7.326"}]];W(i,P({name:"bell"},()=>o,{get iconNode(){return r},children:(n,m)=>{var t=w(),a=N(t);A(a,()=>e.children??M),h(n,t)},$$slots:{default:!0}})),C()}function he(i,e){k(e,!0);/**
+ * @license @lucide/svelte v0.539.0 - ISC
+ *
+ * ISC License
+ *
+ * Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2022 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2022.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */let o=E(e,["$$slots","$$events","$$legacy"]);const r=[["path",{d:"M2 9.5a5.5 5.5 0 0 1 9.591-3.676.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5c0 2.29-1.5 4-3 5.5l-5.492 5.313a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5"}]];W(i,P({name:"heart"},()=>o,{get iconNode(){return r},children:(n,m)=>{var t=w(),a=N(t);A(a,()=>e.children??M),h(n,t)},$$slots:{default:!0}})),C()}function me(i,e){k(e,!0);/**
+ * @license @lucide/svelte v0.539.0 - ISC
+ *
+ * ISC License
+ *
+ * Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2022 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2022.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */let o=E(e,["$$slots","$$events","$$legacy"]);const r=[["path",{d:"M11 20A7 7 0 0 1 9.8 6.1C15.5 5 17 4.48 19 2c1 2 2 4.18 2 8 0 5.5-4.78 10-10 10Z"}],["path",{d:"M2 21c0-3 1.85-5.36 5.08-6C9.5 14.52 12 13 13 12"}]];W(i,P({name:"leaf"},()=>o,{get iconNode(){return r},children:(n,m)=>{var t=w(),a=N(t);A(a,()=>e.children??M),h(n,t)},$$slots:{default:!0}})),C()}function _e(i,e){k(e,!0);/**
+ * @license @lucide/svelte v0.539.0 - ISC
+ *
+ * ISC License
+ *
+ * Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2022 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2022.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */let o=E(e,["$$slots","$$events","$$legacy"]);const r=[["path",{d:"M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"}],["path",{d:"M16 3.128a4 4 0 0 1 0 7.744"}],["path",{d:"M22 21v-2a4 4 0 0 0-3-3.87"}],["circle",{cx:"9",cy:"7",r:"4"}]];W(i,P({name:"users"},()=>o,{get iconNode(){return r},children:(n,m)=>{var t=w(),a=N(t);A(a,()=>e.children??M),h(n,t)},$$slots:{default:!0}})),C()}var ge=H("<li> </li>"),pe=H('<div><h2><!> </h2> <ul class="list-disc list-inside text-gray-700 space-y-2"></ul></div>'),xe=H('<section class="bg-gray-50 py-12 min-h-screen"><div class="max-w-6xl mx-auto px-4"><h1 class="text-4xl font-extrabold text-emerald-800 mb-12 text-center">「都市と自然を、やさしくむすぶ」三蔵新山株式会社の取り組み</h1> <div class="space-y-6"></div></div></section>');function Se(i){const e=[{title:"【三新（SS）鉄道からのお知らせ】",type:"alert",icon:ue,content:["車両を撮影される方は以下のルールをお守りください","① 屋根の上に登る、黄色い線から飛び出るなどの行為は大変危険ですのでお辞めください","② 他のお客様の迷惑になるような行為はお辞めください","③ 上記の行為を発見した場合係員が駆けつけます","危険行為を発見した場合はお近くの通報ボタンでお知らせください","私たちはお客様が快適に使える鉄道会社を目指しております。ご理解よろしくお願いします"]},{title:"環境への配慮",icon:me,content:["駅や車両での省エネルギー化を推進し、CO2排出量削減に取り組んでいます。","緑あふれる駅周辺整備や、花壇・緑地の保全を行い、自然と共生する街づくりを支援。"]},{title:"地域との連携",icon:_e,content:["地元の祭りやイベントに積極的に参加し、地域の文化や人と鉄道をつなげています。","地元産品を使った駅弁やお土産を開発し、地域経済の活性化を図っています。"]},{title:"お客様へのやさしさ",icon:he,content:["バリアフリー対応の駅設計やスタッフ研修を行い、誰もが安心して利用できる環境を整備。","快適な乗車環境づくりやサービス向上に日々努めています。"]}];var o=xe(),r=b(o),n=S(b(r),2);z(n,5,()=>e,B,(m,t)=>{var a=pe(),l=b(a),s=b(l);{var x=f=>{var v=w(),p=N(v);ce(p,()=>u(t).icon,(y,T)=>{T(y,{class:"w-6 h-6"})}),h(f,v)};ie(s,f=>{u(t).icon&&f(x)})}var d=S(s);g(l);var c=S(l,2);z(c,5,()=>u(t).content,B,(f,v)=>{var p=ge(),y=b(p,!0);g(p),R(()=>V(y,u(v))),h(f,p)}),g(c),g(a),R(()=>{O(a,1,`rounded-2xl p-6 shadow-lg transition-transform hover:-translate-y-1 hover:shadow-xl
+    ${u(t).type==="alert"?"bg-yellow-100 border-2 border-yellow-400":"bg-white"}`),O(l,1,`flex items-center gap-3 text-2xl font-semibold mb-4
+        ${u(t).type==="alert"?"text-yellow-800":"text-emerald-700"}`),V(d,` ${u(t).title??""}`)}),h(m,a)}),g(n),g(r),g(o),h(i,o)}export{Se as component};
